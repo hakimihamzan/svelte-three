a62c9cd3dec8f16fb9c0c888d7c9f67d3d869ad3
HakimiHamzan <hakimihamzan20@gmail.com>
2021-09-26 01:27:46 +0800

    edit content 1

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Man im having so much fun here, gh pages is free</p>
 </main>
 
 <style>
